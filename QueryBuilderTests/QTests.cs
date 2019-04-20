19e53b46b3497047f4c01bc3c4e4fedcf4b1d217
ChiefTwoPencils <robertwilk916@gmail.com>
2019-04-20 09:24:38 -0700

    Added Q tests.

--- QueryBuilderTests/QTests.cs
+++ QueryBuilderTests/QTests.cs
@@ -1,7 +1,25 @@
+using QueryBuilder;
+using Xunit;
+
 namespace QueryBuilderTests
 {
     public class QTests
     {
-        
+        [Fact]
+        public void VTest_ShouldReturnEmptyV()
+        {
+            const string expected = "V()";
+            var actual = Q.V();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void VTest_ShouldReturnVWithId()
+        {
+            const int id = 42;
+            var expected = $"V({id.ToString()})";
+            var actual = Q.V(id);
+            Assert.Equal(expected, actual);
+        }
     }
 }
