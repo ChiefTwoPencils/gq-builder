4e1b68127b0a6c58ef3a8c6873cfb9803e1da046
ChiefTwoPencils <robertwilk916@gmail.com>
2019-04-19 23:36:06 -0700

    Added lots of convenience and Q (__).

--- QueryBuilder/QueryBuilder.cs
+++ QueryBuilder/QueryBuilder.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Text;
 
 namespace QueryBuilder
@@ -6,23 +7,57 @@ namespace QueryBuilder
     {
         private StringBuilder Builder { get; }
 
-        private QueryBuilder()
+        private QueryBuilder() : this("g") { }
+
+        internal QueryBuilder(string firstStep)
         {
-            Builder = new StringBuilder("g");
+            Builder = new StringBuilder(firstStep);
         }
         
         public static QueryBuilder Create() => new QueryBuilder();
 
-        public QueryBuilder V() => Append(".V()");
-
-        public QueryBuilder V(long id) => Append($".V({id})");
-
-        public QueryBuilder HasLabel(string label) => Append($".hasLabel({label})");
-
-        private QueryBuilder Append(string step)
+        public static QueryBuilder Empty()
         {
-            Builder.Append(step);
+            var qb = Create();
+            qb.Builder.Clear();
+            return qb;
+        }
+
+        public QueryBuilder V() => Append("V");
+
+        public QueryBuilder V(long id) => Append("V", id.ToString());
+
+        public QueryBuilder HasLabel(string label) => Append($"hasLabel", label);
+
+        public QueryBuilder As(string label) => Append($"as", label);
+
+        public QueryBuilder Select(string label) => Append($"select", label);
+
+        public QueryBuilder Select(string label, params string[] labels)
+        {
+            var @params = labels
+                .Aggregate(new StringBuilder(label), (builder, s) => builder.Append($", {s}"));
+            return Select(@params.ToString());
+        }
+
+        public override string ToString() => Builder.ToString();
+        
+        private QueryBuilder Append(string step, string value = "")
+        {
+            Builder.Append($".{Q.Append(step, value)}");
             return this;
         }
+
+        public static implicit operator string(QueryBuilder qb) => qb.ToString();
+    }
+
+    public static class Q
+    {
+        private static QueryBuilder Builder(string step, string value = "")
+            => new QueryBuilder(Append(step, value));
+        public static QueryBuilder V() => Builder("V");
+        public static QueryBuilder V(long id) => Builder("V", id.ToString());
+
+        public static string Append(string step, string value) => $"{step}({value})";
     }
 }
